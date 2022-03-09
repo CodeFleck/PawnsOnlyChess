546c51bf2858683aefafbd27050cda67bf17f5ad
DFleck <danielfleck@live.ca>
2022-03-09 17:11:29 -0300

    Update second stage

--- Pawns-Only Chess/task/src/chess/Main.kt
+++ Pawns-Only Chess/task/src/chess/Main.kt
@@ -39,42 +39,50 @@ fun main() {
     //print board
     printBoard(board)
 
-    var player1Input: String = ""
-    var player2Input: String = ""
+    var player1Input: String
+    var player2Input: String
 
     var isValid = false
-    var isExit = false
+    var isTheEnd = false
     do {
         do {
-            println(player1 + "'s turn:")
+            println("$player1\'s turn:")
             player1Input = scanner.next()
-            var isExit = validateExitInput(player1Input)
-            isValid = validatePlayerInput(player1Input)
+            val isExit = validateExitInput(player1Input)
+            if (isExit) {
+                isValid = false
+                isTheEnd = true
+            }
             if (!isExit) {
+                isValid = validatePlayerInput(player1Input)
                 if (isValid) {
-                    println(player1Input)
+
                 } else {
                     println("Invalid Input")
                 }
             }
         } while (!isValid && !isExit)
 
-        if (!isExit) {
+        if (!isTheEnd) {
             do {
-                println(player2 + "'s turn:")
+                println("$player2's turn:")
                 player2Input = scanner.next()
-                var isExit = validateExitInput(player2Input)
-                isValid = validatePlayerInput(player2Input)
+                val isExit = validateExitInput(player2Input)
+                if (isExit) {
+                    isValid = false
+                    isTheEnd = true
+                }
                 if (!isExit) {
+                    isValid = validatePlayerInput(player2Input)
                     if (isValid) {
-                        println(player2Input)
+
                     } else {
                         println("Invalid Input")
                     }
                 }
             } while (!isValid && !isExit)
         }
-    } while (!isExit)
+    } while (!isTheEnd)
     println("Bye!")
 
 }
@@ -87,19 +95,21 @@ fun validateExitInput(input: String): Boolean {
 }
 
 fun validatePlayerInput(input: String): Boolean {
-    val validCharInputs = listOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
-    val validNumberInputs = listOf(1, 2, 3, 4, 5, 6, 7, 8)
-    var firstLetter = input.substring(0, 1)
-    var firstNumber = input.substring(1, 2)
-    var secondLetter = input.substring(2, 3)
-    var secondNumber = input.substring(3, 4)
 
-    if (validCharInputs.contains(
-            firstLetter.toCharArray().get(0)
-        ) && validCharInputs.contains(secondLetter.toCharArray().get(0))
-        && validNumberInputs.contains(firstNumber.toInt()) && validNumberInputs.contains(secondNumber.toInt())
-    ) {
-        return true;
+    if (input.length != 4) return false
+
+    val firstLetter = input.substring(0, 1).single()
+    val firstNumber = input.substring(1, 2).toInt()
+    val secondLetter = input.substring(2, 3).single()
+    val secondNumber = input.substring(3, 4).toInt()
+
+    val isFirstLetterWithin = firstLetter in 'a'..'h'
+    val isSecondLetterWithin = secondLetter in 'a'..'h'
+    val isFirstNumberWithin = firstNumber in 1..8
+    val isSecondNumberWithin = secondNumber in 1..8
+
+    if (isFirstLetterWithin && isSecondLetterWithin && isFirstNumberWithin && isSecondNumberWithin) {
+        return true
     }
     return false
 }
